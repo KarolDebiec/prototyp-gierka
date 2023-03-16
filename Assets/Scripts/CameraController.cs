eb62782b1d1ae295b55c9da85abbb7a4e9a51f15
Karol D <karold@tutanota.com>
2023-03-16 07:15:48 +0100

    camera and speed adjustment

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,10 +8,20 @@ public class CameraController : MonoBehaviour
     public GameObject player2;
     public Vector3 offset;
     public float disMultiplier;
-
+    public float minDistance;
+    private float dist;
     void Update()
     {
-       // Vector3.Distance(player1.transform.position, player2.transform.position);
-        transform.position = offset + ((player1.transform.position+ player2.transform.position)/2);
+        dist = Vector3.Distance(player1.transform.position, player2.transform.position);
+        if (dist < minDistance)
+        {
+            disMultiplier = 1;
+        }
+        else
+        {
+            disMultiplier = dist/minDistance;
+        }
+        //Debug.Log(Vector3.Distance(player1.transform.position, player2.transform.position));
+        transform.position = offset * disMultiplier + ((player1.transform.position+ player2.transform.position)/2);
     }
 }
